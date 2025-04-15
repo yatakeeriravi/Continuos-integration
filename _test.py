f80ea382bb54d4f1096558db0ce6c6b31ed03ab0
yatakeeriravi <yatakeeri.ravikiran>
2025-04-15 13:15:05 +0530

    CI run - 2

--- _test.py
+++ _test.py
@@ -14,7 +14,7 @@ def fifth_power(n):
 
 # Testing the square function
 def test_square():
-    assert square(2) == 4, "Test Failed: Square of 2 should be 4"
+    assert square(2) == 5, "Test Failed: Square of 2 should be 4"
     assert square(3) == 9, "Test Failed: Square of 3 should be 9"
 
 # Testing the cube function
